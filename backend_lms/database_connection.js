e96d2f156d47e63457527402dbd86b4873bef125
Nakesh_Tewari <nakesh1107@gmail.com>
2026-03-29 23:44:53 +0530

    fix db connection port

--- backend_lms/database_connection.js
+++ backend_lms/database_connection.js
@@ -12,6 +12,7 @@ try {
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
+    port: process.env.DATABASE_PORT || 3306,
   });
 
   console.log("Connected to MySQL database");
